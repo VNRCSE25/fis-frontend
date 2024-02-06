d791ffa556fcebd41f260fe746870c87482249d4
shashidhar7682 <shashidhar7682@gmail.com>
2024-02-06 14:10:03 +0530

    commit_backend_1.4.5

--- src/pages/loginPage/ResetPassword/ResetPassword.jsx
+++ src/pages/loginPage/ResetPassword/ResetPassword.jsx
@@ -115,8 +115,12 @@ function ResetPassword() {
 
   const handleResetPassword = async () => {
     try {
+      if(newPassword!==confirmNewPassword){
+        toast.error("Passwords do not match");
+        return null;
+      }
       const response = await axios.put(
-        `https://fmp-mongodb-api.onrender.com/user-api/change-password-with-otp/${currentUser.username}`,
+        `https://fmp-mongodb-api.onrender.com/user-api/change-password-with-otp/${username}`,
         { newPassword }
       );
       toast.success(response.data.message);
@@ -324,9 +328,27 @@ function ResetPassword() {
                     type="password"
                     id="newPassword"
                     className="search-input form-control me-2"
-                    placeholder="Password"
+                    placeholder="New Password"
                     value={newPassword}
                     onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+              <label
+                htmlFor="confirmNewPassword"
+                className="text-white fw-bold fs-5 p-2"
+              >
+                Confirm New Password:
+              </label>
+              <div className="row">
+                <div className="col-lg-4 col-sm-6 col-md-6 m-2">
+                  <input
+                    type="password"
+                    id="confirmNewPassword"
+                    className="search-input form-control me-2"
+                    placeholder="Confirm New Password"
+                    value={confirmNewPassword}
+                    onChange={(e) => setConfirmNewPassword(e.target.value)}
                   />
                 </div>
               </div>
